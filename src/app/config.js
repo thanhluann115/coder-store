9c2685832829595d68407a185b2b457e942fbee2
Thanh Luan <thanhluan113@gmail.com>
2026-06-17 23:23:28 +0700

    Update URL

--- src/app/config.js
+++ src/app/config.js
@@ -1,4 +1,3 @@
-//export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-//console.log("BASE_URL =", BASE_URL);//
-export const BASE_URL = "http://127.0.0.1:5000";
+console.log("BASE_URL =", BASE_URL);
